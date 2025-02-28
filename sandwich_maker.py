2dfab8463bd32c3bab5d26c3d61986aa9008069e
Alejandro <pmurillo@charlotte.edu>
2025-02-28 17:53:46 -0500

    Implemented the check_resources method.

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -5,7 +5,13 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+      #Loop through each of the required ingredients
+        for item, required_amount in ingredients.items():
+            # Check if Machine has enough resources
+            if self.machine_resources[item] < required_amount:
+                print(f"Sorry, not enough {item}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+       """Deduct required ingredients from resources to make the sandwich."""
